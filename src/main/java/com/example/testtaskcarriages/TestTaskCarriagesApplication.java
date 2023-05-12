7ac080ea704ff059b082777a4fdd677b26629a5c
Евгений <ugend84@yandex.ru>
2023-05-12 12:46:04 +0300

    Initial commit

---
+++ src/main/java/com/example/testtaskcarriages/TestTaskCarriagesApplication.java
@@ -0,0 +1,13 @@
+package com.example.testtaskcarriages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskCarriagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskCarriagesApplication.class, args);
+	}
+
+}
